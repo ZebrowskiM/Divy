6e749dc10d95a4b79caa5814d1ed98e7993f9271
Marek Zebrowski <mzebrowski@docuphase.com>
2020-08-28 15:55:00 -0400

    Adding Pocos and app config

---
+++ Divy.Common/ObjectBase.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace Divy.Common
+{
+    public class ObjectBase
+    {
+        public override string ToString()
+        {
+            var stringBuilder = new StringBuilder();
+            foreach (PropertyDescriptor descriptor in TypeDescriptor.GetProperties(this))
+            {
+                var name = descriptor.Name;
+                var value = descriptor.GetValue(this);
+                stringBuilder.Append($" {name} = {value} ");
+
+            }
+            return stringBuilder.ToString();
+        }
+    }
+}
---
+++ Divy.Common/POCOs/Share.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Divy.Common.POCOs
+{
+    public class Share : ObjectBase
+    {
+
+    }
+}
