aa722cd1bd7479c6090830f2cbab110d5be83ff9
ZebrowskiM <zebrowskimarek00@gmail.com>
2020-08-31 12:59:28 -0400

    Filled in ctor and updated path for trace

--- Divy.DAL.Sqlite/SqliteWatchListAdapter.cs
+++ Divy.DAL.Sqlite/SqliteWatchListAdapter.cs
@@ -1,13 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SQLite;
+using System.IO;
 using System.Text;
 using Divy.Common.POCOs;
 using Divy.DAL.Interfaces;
 
 namespace Divy.DAL.Sqlite
 {
-    class SqliteWatchListAdapter : IWatchListAdapter
+    public class SqliteWatchListAdapter : IWatchListAdapter
     {
+        private readonly string _folderPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData), "Divy");
+        private readonly string _dbName = "DivyBase.db";
+        private readonly string _connectionString;
+        public SqliteWatchListAdapter()
+        {
+            var path = Path.Combine(_folderPath, _dbName);
+            if(!File.Exists(path))
+                SQLiteConnection.CreateFile(path);
+            _connectionString = $"DataSource={path};Version=3;";
+
+        }
         public int CreateWatchList(WatchList watchList)
         {
             throw new NotImplementedException();
